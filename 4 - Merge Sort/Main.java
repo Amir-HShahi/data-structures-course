fe1acb5c823d2ee359ccc20c9f436b04b6e0a4d2
Amir Hasanshahi <157722620+Amir-HShahi@users.noreply.github.com>
2024-10-24 13:48:17 +0330

    Delete Merge Sort directory

--- 4 - Merge Sort/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int[] a = {1,2,6};
-        int[] b = {3,4,5};
-        for (int i : Sort.merge(b, a)) {
-            System.out.println(i);
-        }
-    }
-}
