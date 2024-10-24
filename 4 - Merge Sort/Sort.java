63edaf93a3d9fc68cf7e46763d024152bc6104c8
Amir-HShahi <amir13831107h@gmail.com>
2024-10-24 14:03:01 +0330

    init merge sort

---
+++ 4 - Merge Sort/Sort.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Sort {
+    public static <T extends Comparable<T>> void mergeSort() {
+
+    }
+
+    private static <T extends Comparable<T>> void merge(List<T> list, int start, int end, int mid) {
+        
+    }
+}
